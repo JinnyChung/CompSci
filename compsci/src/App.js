bd9808a9280fcfeb1dd75114bff28e44e7208538
abhimanyubangaru <abhi.bangaru24@gmail.com>
2021-04-14 10:21:34 -0700

    2nd Commit
    Messages work. Verification up next

--- compsci/src/App.js
+++ compsci/src/App.js
@@ -7,7 +7,7 @@ import ValidUsers from './components/ValidUsers'
 function App() {
 
   const [verified, setV] = useState(false);//is the message by a verified user
-  const[showAddMessage, setShowAddMessage] = useState(false); //toggle between being able to add a message or not for design 
+  const[showAddMessage, setShowAddMessage] = useState(false); //toggle between being able to add a message or not for design
   const [messages,setMessages] = useState([
     {
       message: "Hello",
@@ -20,26 +20,26 @@ function App() {
   ]) // starting of array with messages
 
   const addMessage = (message) => {
-    if(verified){
+
     const id = Math.floor(Math.random() * 10000) + 1
     const newMessage = {id, ...message}
     setMessages([newMessage, ...messages])
-    }
+
     setV(false);
-  }//if the post is by a verified user, add it to the messages array at the top 
-  
- 
+  }//if the post is by a verified user, add it to the messages array at the top
+
+
 
   return (
     <div className="App">
       <Button text="Toggle New Announcement" onClick={() => setShowAddMessage(!showAddMessage)}/> {/*button to toggle announcements, passes the prop to toggle the add message screen */}
       {showAddMessage && <Announcements onAdd={addMessage} tryVerify={ ()=> setV(!verified)} />}{/*trying to set up verification process */}
-          {messages.map((message,index) => ( 
+          {messages.map((message,index) => (
         <Announcement message={message.message} person={message.person}/>
 
       ))} {/*map out all of the messages from the array*/}
 
-      
+
     </div>
   );
 }
